b418c301201b617db815f659c3528efc2abd6fc1
Carlo Mabugay <chikolokoy08@gmail.com>
2025-01-26 11:45:30 +0800

    Added .env on ignore files and changes db connection to env based.

--- lan-chat-backend/models/db.js
+++ lan-chat-backend/models/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'P@s5w0d4dm18',
-    database: 'lan_chat_app',
+    host: process.env.DBHOST ? process.env.DBHOST : '127.0.0.1',
+    user: process.env.DBUNAME ? process.env.DBUNAME : 'root',
+    password: process.env.DBPASS ? process.env.DBPASS : 'yourpasword',
+    database: process.env.DBNAME ? process.env.DBNAME : 'dbname',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
